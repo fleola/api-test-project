6fef2625950ec35258ee1dab65f21f74dbe39442
fleola <feola.leopoldo@gmail.com>
2022-08-24 16:03:12 +0200

    GET /planet with mock for testing

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
     testEnvironment: "node",
     verbose: true,
     preset: "ts-jest",
+    clearMocks: true,
+    setupFilesAfterEnv: ["./src/lib/prisma/client.mock.ts"],
 };
--- src/server.test.ts
+++ src/server.test.ts
@@ -1,16 +1,38 @@
 import supertest from "supertest";
-
+import { prismaMock } from "./lib/prisma/client.mock"
 import app from "./app"
 
 const request = supertest(app)
+
 test("GET /planets", async () => {
+    const planets = [
+        {
+            "id": 1,
+            "name": "Mercury",
+            "description": null,
+            "diameter": 1234,
+            "moon": 12,
+            "createdAt": "2022-08-24T13:44:38.572Z",
+            "updatedAt": "2022-08-24T13:44:25.208Z"
+        },
+        {
+            "id": 2,
+            "name": "Venus",
+            "description": null,
+            "diameter": 5678,
+            "moon": 2,
+            "createdAt": "2022-08-24T13:44:50.269Z",
+            "updatedAt": "2022-08-24T13:44:40.430Z"
+        }
+    ]
+
+    //@ts-ignore
+    prismaMock.planet.findMany.mockResolvedValue(planets)
+
     const response = await request
         .get("/planets")
         .expect(200)
         .expect("Content-Type", /application\/json/)
 
-    expect(response.body).toEqual([
-        { name: "Mercury" },
-        { name: "Venus" }
-    ])
+    expect(response.body).toEqual(planets)
 })
