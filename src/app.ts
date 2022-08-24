da57541a270dfbfc675696145d227f56e8c7aa35
fleola <feola.leopoldo@gmail.com>
2022-08-24 19:08:45 +0200

    valid request test not working

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express from "express"
 import "express-async-errors"
 import prisma from "./lib/prisma/client"
+import {
+    validate,
+    PlanetData,
+    planetSchema,
+    validationErrorMiddleware
+} from "./lib/validation"
 
 const app = express()
 
@@ -12,10 +18,12 @@ app.get("/planets", async (request, response) => {
     response.json(planets)
 })
 
-app.post("/planets", async (request, response) => {
-    const planet = request.body
+app.post("/planets", validate({ body: planetSchema }), async (request, response) => {
+    const planet: PlanetData = request.body
 
     response.status(201).json(planet)
-})
+});
+
+app.use(validationErrorMiddleware);
 
 export default app
--- src/server.test.ts
+++ src/server.test.ts
@@ -3,53 +3,71 @@ import { prismaMock } from "./lib/prisma/client.mock"
 import app from "./app"
 
 const request = supertest(app)
+describe("GET /planets", () => {
+    test("Valid Request", async () => {
+        const planets = [
+            {
+                "id": 1,
+                "name": "Mercury",
+                "description": null,
+                "diameter": 1234,
+                "moon": 12,
+                "createdAt": "2022-08-24T13:44:38.572Z",
+                "updatedAt": "2022-08-24T13:44:25.208Z"
+            },
+            {
+                "id": 2,
+                "name": "Venus",
+                "description": null,
+                "diameter": 5678,
+                "moon": 2,
+                "createdAt": "2022-08-24T13:44:50.269Z",
+                "updatedAt": "2022-08-24T13:44:40.430Z"
+            }
+        ]
 
-test("GET /planets", async () => {
-    const planets = [
-        {
-            "id": 1,
-            "name": "Mercury",
-            "description": null,
-            "diameter": 1234,
-            "moon": 12,
-            "createdAt": "2022-08-24T13:44:38.572Z",
-            "updatedAt": "2022-08-24T13:44:25.208Z"
-        },
-        {
-            "id": 2,
-            "name": "Venus",
-            "description": null,
-            "diameter": 5678,
-            "moon": 2,
-            "createdAt": "2022-08-24T13:44:50.269Z",
-            "updatedAt": "2022-08-24T13:44:40.430Z"
-        }
-    ]
+        //@ts-ignore
+        prismaMock.planet.findMany.mockResolvedValue(planets)
 
-    //@ts-ignore
-    prismaMock.planet.findMany.mockResolvedValue(planets)
+        const response = await request
+            .get("/planets")
+            .expect(200)
+            .expect("Content-Type", /application\/json/)
 
-    const response = await request
-        .get("/planets")
-        .expect(200)
-        .expect("Content-Type", /application\/json/)
-
-    expect(response.body).toEqual(planets)
+        expect(response.body).toEqual(planets)
+    })
 })
 
-test("POST /planets", async () => {
-    const planet =
-    {
-        "name": "Mercury",
-        "diameter": 1234,
-        "moon": 12,
-    }
+describe("POST /planets", () => {
+    test("Valid Request", async () => {
+        const planet = {
+            name: "Mercury",
+            diameter: 1234,
+            moon: 12,
+        };
 
-    const response = await request
-        .post("/planets")
-        .send(planet)
-        .expect(201)
-        .expect("Content-Type", /application\/json/)
+        const response = await request
+            .post("/planets")
+            .send(planet)
+            .expect(201)
+            .expect("Content-Type", /application\/json/)
 
-    expect(response.body).toEqual(planet)
+        expect(response.body).toEqual(planet)
+    })
+    test("Invalid Request", async () => {
+        const planet = {
+            diameter: 1234,
+            moons: 12,
+        }
+        const response = await request
+            .post("/planets")
+            .send(planet)
+            .expect(422)
+            .expect("Content-Type", /application\/json/)
+        expect(response.body).toEqual({
+            errors: {
+                body: expect.any(Array)
+            }
+        })
+    })
 })
