6ece061692a75f23e7064b30742513c55052d5fd
fleola <feola.leopoldo@gmail.com>
2022-08-30 16:16:44 +0200

    github oauth app created

--- src/lib/middleware/passport.ts
+++ src/lib/middleware/passport.ts
@@ -1,11 +1,12 @@
 import passport from "passport";
 import passportGithub2 from "passport-github2"
+import config from "../../config";
 
 const githubStrategy = new passportGithub2.Strategy(
     {
-        clientID: "",
-        clientSecret: "",
-        callbackURL: ""
+        clientID: config.GITHUB_CLIENT_ID,
+        clientSecret: config.GITHUB_CLIENT_SECRET,
+        callbackURL: config.GITHUB_CALLBACK_URL
     },
     function (
         accessToken: string,
